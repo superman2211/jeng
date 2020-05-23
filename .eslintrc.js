26cc7c16b7b0f2f0b37d7f071d5940c1bd292845
Sergey Antonov <superflasher22@gmail.com>
2020-05-23 15:45:53 +0300

    Init mono repo

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {},
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+	'no-useless-constructor': 'off',
+    'no-tabs': 'off',
+    'max-len': 'off',
+    'no-plusplus': 'off',
+    'no-bitwise': 'off',
+	'no-param-reassign': 'off',
+	'no-throw-literal': 'off',
+	'no-underscore-dangle': 'off',
+	'no-case-declarations': 'off',
+    'lines-between-class-members': 'off',
+    'import/extensions': 'off',
+	'indent': ['error', 'tab', { 'SwitchCase': 1 }],
+	'@typescript-eslint/type-annotation-spacing': ['error', { 'before': false, 'after': true, overrides: { arrow: { before: true, after: true } } }],
+  },
+  ignorePatterns: ['dist', 'node_modules'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+};
