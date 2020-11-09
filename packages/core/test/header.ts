3759fa46fc0d1e84f5f01dde5a44141d7be54062
Sergey Antonov <superflasher22@gmail.com>
2020-11-09 19:25:37 +0300

    Update sample

--- packages/core/test/header.ts
+++ packages/core/test/header.ts
@@ -1,8 +1,9 @@
 import { Text } from '../src/extensions/Text';
 import { Image } from '../src/extensions/Image';
 import { Container } from '../src/core/Container';
+import { Transform } from '../src/enigne2d/Transform';
 
-interface Header extends Container {
+interface Header extends Container, Transform {
 
 }
 
@@ -14,9 +15,24 @@ interface HeaderProperties {
 export default function header(props: HeaderProperties): Header {
 	return {
 		type: 'container',
+		x: 100,
+		y: 200,
 		children: [
-			{ type: 'text', text: props.title, onClick: props.onClick } as Text,
-			{ type: 'image', src: 'logo.png' } as Image,
+			{
+				type: 'text',
+				text: props.title,
+				onClick: props.onClick,
+				x: 10,
+				y: 20,
+				scaleX: 2,
+			} as Text,
+			{
+				type: 'image',
+				src: 'logo.png',
+				x: 1,
+				y: 2,
+				rotation: 0.1,
+			} as Image,
 		],
 	};
 }
