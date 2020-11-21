3f9a92e3b48fc0ba768c16f50a667761c45773cc
Sergey Antonov <superflasher22@gmail.com>
2020-11-21 21:14:29 +0300

    Correct exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,16 +1,10 @@
-import { Container, CONTAINER } from './core/Container';
-import { Image, IMAGE } from './enigne2d/Image';
-import { Text, TEXT } from './enigne2d/Text';
 import Engine2d from './enigne2d/Engine2d';
-import { Transform } from './enigne2d/Transform';
+
+export { Container, CONTAINER } from './core/Container';
+export { Image, IMAGE } from './enigne2d/Image';
+export { Text, TEXT } from './enigne2d/Text';
+export { Transform } from './enigne2d/Transform';
 
 export {
-	CONTAINER,
-	IMAGE,
-	TEXT,
 	Engine2d,
-	Container,
-	Image,
-	Text,
-	Transform,
 };
