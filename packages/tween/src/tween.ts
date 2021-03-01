c8f71ba8143290bd73f09f009d3127a53f3b718c
Sergey Antonov <superflasher22@gmail.com>
2021-03-01 18:05:14 +0300

    Tween refactoring

--- packages/tween/src/tween.ts
+++ packages/tween/src/tween.ts
@@ -1,6 +1,4 @@
-/* eslint-disable class-methods-use-this */
-/* eslint-disable no-console */
-import { Component, UpdateContext, Support } from '@e2d/engine';
+import { Component, Engine } from '@e2d/core';
 import { easing, LINEAR } from './easing';
 
 export const TWEEN = 'tween';
@@ -28,79 +26,81 @@ export interface Tween {
 	}
 }
 
-export function updateTweem(component: Component, context: UpdateContext) {
-	const { tween } = component as any as Tween;
-	const { phases } = tween;
+export namespace TweenExtension {
+	export function update(component: Component, engine: Engine) {
+		const { tween } = component as any as Tween;
+		const { phases } = tween;
 
-	if (!phases?.length) {
-		console.warn('Animation parts not found');
-		return;
-	}
-
-	if (tween.time === undefined) {
-		tween.time = 0;
-	}
-	tween.time += context.time;
-	const { time } = tween;
-
-	let offset: number = 0;
-
-	phases.forEach((phase) => {
-		if (!phase.time) {
-			console.warn('Animation time not set');
+		if (!phases?.length) {
+			engine.debug.warning('Animation parts not found');
 			return;
 		}
 
-		if (phase.offset !== undefined) {
-			offset = phase.offset;
+		if (tween.time === undefined) {
+			tween.time = 0;
 		}
+		tween.time += engine.updater.time;
+		const { time } = tween;
 
-		const easingName = phase?.easing?.toLowerCase() ?? LINEAR;
-		const easingMethod = (easing as any)[easingName];
-		if (!easingMethod) {
-			console.warn(`Unknown easing type: ${easing}`);
-			return;
-		}
+		let offset: number = 0;
 
-		const value = (time - offset) / phase.time;
-
-		offset += phase.time;
-
-		if (value < 0 || value > 1) {
-			return;
-		}
-		const easingValue = easingMethod(value);
-
-		if (!phase.to) {
-			console.warn('Animation "to" state not found');
-			return;
-		}
-
-		if (!phase.from) {
-			phase.from = {};
-		}
-
-		const { to, from } = phase;
-		const state = component as any as TweenState;
-
-		Object.keys(to).forEach((key) => {
-			if (from[key] === undefined) {
-				from[key] = state[key] ?? 0;
+		phases.forEach((phase) => {
+			if (!phase.time) {
+				engine.debug.warning('Animation time not set');
+				return;
 			}
-			const fromValue = from[key];
-			const toValue = to[key];
-			if (typeof fromValue === 'number' && typeof toValue === 'number') {
-				const stateValue = fromValue + easingValue * (toValue - fromValue);
-				state[key] = stateValue;
+
+			if (phase.offset !== undefined) {
+				offset = phase.offset;
 			}
+
+			const easingName = phase?.easing?.toLowerCase() ?? LINEAR;
+			const easingMethod = (easing as any)[easingName];
+			if (!easingMethod) {
+				engine.debug.warning(`Unknown easing type: ${easing}`);
+				return;
+			}
+
+			const value = (time - offset) / phase.time;
+
+			offset += phase.time;
+
+			if (value < 0 || value > 1) {
+				return;
+			}
+			const easingValue = easingMethod(value);
+
+			if (!phase.to) {
+				engine.debug.warning('Animation "to" state not found');
+				return;
+			}
+
+			if (!phase.from) {
+				phase.from = {};
+			}
+
+			const { to, from } = phase;
+			const state = component as any as TweenState;
+
+			Object.keys(to).forEach((key) => {
+				if (from[key] === undefined) {
+					from[key] = state[key] ?? 0;
+				}
+				const fromValue = from[key];
+				const toValue = to[key];
+				if (typeof fromValue === 'number' && typeof toValue === 'number') {
+					const stateValue = fromValue + easingValue * (toValue - fromValue);
+					state[key] = stateValue;
+				}
+			});
 		});
-	});
 
-	if (tween.loop && tween.time > offset) {
-		tween.time = 0;
+		if (tween.loop && tween.time > offset) {
+			tween.time = 0;
+		}
 	}
-}
 
-export function applyTweenExtension(support: Support) {
-	support.propertyHandlers.set(TWEEN, updateTweem);
+	export function init(engine: Engine) {
+		engine.components.properties.set(TWEEN, update);
+	}
 }
