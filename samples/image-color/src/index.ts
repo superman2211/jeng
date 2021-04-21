0f65efb2daf11956526c26b97b8128ce006035bf
Sergey Antonov <superflasher22@gmail.com>
2021-04-21 15:00:48 +0300

    Correct image sample

--- samples/image-color/src/index.ts
+++ samples/image-color/src/index.ts
@@ -14,27 +14,42 @@ engine.ticker.play();
 // setup page
 document.body.style.margin = '0';
 document.body.style.padding = '0';
+document.body.style.backgroundColor = 'grey';
 // add engine view to page
 document.body.appendChild(engine.platform.view);
 
-const ring = {
+// create ring instance
+const ring: Image = {
 	type: 'image',
 	src: 'assets/Rings_promo_mythic.png',
 	scale: 0.4,
-} as Image;
+};
 
+// array of color transformations
 const transforms: Array<Transform> = [
 	{},
 	{ alpha: 0.2 },
 	{ tint: { color: 0xff0000, value: 0.5 } },
 	{ brightness: 0.5 },
-	{ brightness: 0 },
 	{ brightness: -0.5 },
+	{
+		colorTransform: {
+			alphaMultiplier: 1,
+			redMultiplier: 1,
+			greenMultiplier: 0.5,
+			blueMultiplier: 1,
+			alphaOffset: 0,
+			redOffset: 0,
+			greenOffset: 0xff,
+			blueOffset: 0x22,
+		},
+	},
 ];
 
-// create container with image
+// create container with images
 engine.root = {
 	type: 'container',
+	// map transformations to images
 	children: transforms.map((transform, index) => ({
 		...transform,
 		type: 'container',
